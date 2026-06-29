654fa96e1ac6b29af8d497c7998a24a0e6c54df1
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-06-29 17:14:04 +0530

    Create appStore.js for application state management

---
+++ store/appStore.js
@@ -0,0 +1,21 @@
+// store/appStore.js
+
+const appStore = {
+  currentUser: null,
+
+  currentDriver: null,
+
+  currentBooking: null,
+
+  currentScreen: "home",
+
+  language: "hi",
+
+  notifications: [],
+
+  selectedService: null,
+
+  subscription: null,
+};
+
+export default appStore;
