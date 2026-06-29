e7c45a64dba2482f16f89d9b21b760a464d7ffe5
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-06-29 21:42:02 +0530

    Add appConfig.js with application configuration settings

---
+++ config/appConfig.js
@@ -0,0 +1,41 @@
+// config/appConfig.js
+
+export const APP_CONFIG = {
+
+  APP_NAME: "KisanSetu",
+
+  VERSION: "2.0.0",
+
+  COUNTRY_CODE: "+91",
+
+  DEFAULT_LANGUAGE: "hi",
+
+  SUPPORT_EMAIL: "support@kisansetu.in",
+
+  SUPPORT_PHONE: "+919999999999",
+
+  SUBSCRIPTION_RATE: 550,
+
+  MAX_UPLOAD_SIZE: 5 * 1024 * 1024,
+
+  ALLOWED_IMAGE_TYPES: [
+
+    "image/jpeg",
+
+    "image/png",
+
+    "image/webp",
+
+  ],
+
+  ALLOWED_DOCUMENT_TYPES: [
+
+    "application/pdf",
+
+    "image/jpeg",
+
+    "image/png",
+
+  ],
+
+};
