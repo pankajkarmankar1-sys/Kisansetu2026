a960c2588b70b25408093136ae05f861de3f9b21
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-06-29 15:28:37 +0530

    Add Loader component for loading state

---
+++ components/Loader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Loader() {
+  return (
+    <div
+      style={{
+        padding: 20,
+        textAlign: "center",
+      }}
+    >
+      Loading...
+    </div>
+  );
+}
