54d83a09b57e95f7f98aca9bca57593d704861a8
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-06-29 16:52:53 +0530

    Add default database structure in defaultDB.js

---
+++ data/defaultDB.js
@@ -0,0 +1,16 @@
+// data/defaultDB.js
+
+const defaultDB = {
+  users: {},
+  bookings: [],
+  drivers: {},
+  chats: {},
+  khets: {},
+  reviews: {},
+  fleet: {},
+  waitList: [],
+  transfers: [],
+  schedules: {},
+};
+
+export default defaultDB;
