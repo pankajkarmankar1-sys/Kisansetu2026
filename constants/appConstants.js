9146a8f678a0ce08b2e9dd1f5b9fd8d61d4a44ab
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-06-30 07:50:05 +0530

    Add app constants for application configuration

---
+++ constants/appConstants.js
@@ -0,0 +1,25 @@
+export const APP_NAME = "KisanSetu";
+
+export const SUPPORT_NUMBER = "+91XXXXXXXXXX";
+
+export const SUBSCRIPTION_RATE = 550;
+
+export const MAX_UPLOAD_SIZE = 10 * 1024 * 1024;
+
+export const ALLOWED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/png",
+  "image/webp",
+];
+
+export const ALLOWED_DOCUMENT_TYPES = [
+  "application/pdf",
+];
+
+export const STORAGE_BUCKETS = {
+  CUSTOMER: "customer-documents",
+  DRIVER: "driver-documents",
+  TRACTOR: "tractor-documents",
+};
+
+export const OTP_LENGTH = 6;
