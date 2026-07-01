de6d5b97076c50dd557dd973c40db967a63cc41a
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-07-01 20:38:20 +0530

    Update ChatMessage.jsx

--- components/ chat/ /ChatMessage.jsx
+++ components/ chat/ /ChatMessage.jsx
@@ -2,38 +2,36 @@ import React from "react";
 
 export default function ChatMessage({ message }) {
 
+  const isCustomer = message?.sender === "customer";
+
   return (
 
     <div
       style={{
         marginBottom: 10,
-        textAlign:
-          message.sender === "customer"
-            ? "right"
-            : "left",
+        textAlign: isCustomer ? "right" : "left",
       }}
     >
 
       <div
         style={{
           display: "inline-block",
-          background:
-            message.sender === "customer"
-              ? "#2d8a4e"
-              : "#eeeeee",
-          color:
-            message.sender === "customer"
-              ? "#fff"
-              : "#000",
+          background: isCustomer ? "#2d8a4e" : "#eeeeee",
+          color: isCustomer ? "#fff" : "#000",
           padding: 10,
           borderRadius: 10,
+          maxWidth: "70%",
+          wordBreak: "break-word",
         }}
       >
-        {message.text}
+
+        {message?.text}
 
         <br />
 
-        <small>{message.time}</small>
+        <small>
+          {message?.time || message?.created_at || ""}
+        </small>
 
       </div>
 
