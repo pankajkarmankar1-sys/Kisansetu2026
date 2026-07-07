6821d40e5215a120d7c265746b8bb9d9ce605f61
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-07-07 06:05:58 +0530

    Refactor AdminDashboard layout and structure

--- components/admin/AdminDashboard.jsx
+++ components/admin/AdminDashboard.jsx
@@ -6,13 +6,35 @@ import DriverList from "./DriverList";
 
 export default function AdminDashboard() {
   return (
-    <div style={{ display: "flex" }}>
+    <div
+      style={{
+        display: "flex",
+        minHeight: "100vh",
+        background: "#f4f6f8",
+      }}
+    >
       <AdminSidebar />
 
-      <div style={{ flex: 1, padding: 20 }}>
+      <div
+        style={{
+          flex: 1,
+          padding: 20,
+          overflowY: "auto",
+        }}
+      >
+        <h1 style={{ marginBottom: 20 }}>
+          👨‍💼 Admin Dashboard
+        </h1>
+
         <StatsCards />
-        <BookingList />
-        <DriverList />
+
+        <div style={{ marginTop: 30 }}>
+          <BookingList />
+        </div>
+
+        <div style={{ marginTop: 30 }}>
+          <DriverList />
+        </div>
       </div>
     </div>
   );
