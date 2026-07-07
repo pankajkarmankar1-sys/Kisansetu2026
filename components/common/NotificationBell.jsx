6b35e1432c5dc97e34e240233dab2152b8973b07
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-07-07 18:56:02 +0530

    Refactor NotificationBell component for clarity

--- components/common/NotificationBell.jsx
+++ components/common/NotificationBell.jsx
@@ -1,62 +1,138 @@
 import { useEffect, useState } from "react";
-import { getNotifications } from "../../lib/NotificationService";
+import { getNotifications } from "../../lib/notificationService";
+
 
 export default function NotificationBell({
   user,
   onClick,
 }) {
+
   const [count, setCount] = useState(0);
 
+
+
   async function load() {
+
     try {
-      const data = await getNotifications(user?.id);
-      setCount(data?.length || 0);
+
+      if (!user?.id) {
+
+        setCount(0);
+        return;
+
+      }
+
+
+      const data = await getNotifications(
+        user.id
+      );
+
+
+      setCount(
+        data?.length || 0
+      );
+
 
     } catch (e) {
-      console.error("Notification Error:", e);
+
+      console.error(
+        "Notification Error:",
+        e
+      );
+
     }
+
   }
 
-  useEffect(() => {
+
+
+
+  useEffect(()=>{
+
     load();
 
-    const timer = setInterval(load, 5000);
 
-    return () => clearInterval(timer);
+    const timer = setInterval(
+      load,
+      5000
+    );
 
-  }, [user]);
+
+    return () => {
+      clearInterval(timer);
+    };
+
+
+  },[user]);
+
+
+
 
 
   return (
+
     <button
+
       onClick={onClick}
+
       style={{
-        position: "relative",
-        fontSize: 22,
-        background: "none",
-        border: "none",
-        cursor: "pointer",
-        color: "#fff",
+
+        position:"relative",
+
+        fontSize:22,
+
+        background:"none",
+
+        border:"none",
+
+        cursor:"pointer",
+
+        color:"#fff",
+
       }}
+
     >
+
       🔔
 
-      {count > 0 && (
-        <span
-          style={{
-            position: "absolute",
-            top: -5,
-            right: -5,
-            background: "red",
-            color: "#fff",
-            borderRadius: "50%",
-            padding: "2px 6px",
-            fontSize: 10,
-          }}
-        >
-          {count}
-        </span>
-      )}
+
+      {
+        count > 0 && (
+
+          <span
+
+            style={{
+
+              position:"absolute",
+
+              top:-5,
+
+              right:-5,
+
+              background:"red",
+
+              color:"#fff",
+
+              borderRadius:"50%",
+
+              padding:"2px 6px",
+
+              fontSize:10,
+
+            }}
+
+          >
+
+            {count}
+
+          </span>
+
+        )
+      }
+
+
     </button>
+
   );
+
 }
