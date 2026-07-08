2ec00fb065f668e42edc7f9e6b7251980505d6e2
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-07-08 22:58:20 +0530

    Add chandrapur.js with district and talukas data

---
+++ data/chandrapur.js
@@ -0,0 +1,23 @@
+const chandrapur = {
+  district: "Chandrapur",
+
+  talukas: {
+    Ballarpur: [],
+    Bhadravati: [],
+    Brahmapuri: [],
+    Chandrapur: [],
+    Chimur: [],
+    Gondpipri: [],
+    Jiwati: [],
+    Korpana: [],
+    Mul: [],
+    Nagbhid: [],
+    Pombhurna: [],
+    Rajura: [],
+    Sawali: [],
+    Sindewahi: [],
+    Warora: [],
+  },
+};
+
+export default chandrapur;
