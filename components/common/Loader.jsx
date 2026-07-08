a39e32ea0db66735f795865c3ccf455dce868c6e
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-07-08 17:16:35 +0530

    Update Loader component styling

--- components/common/Loader.jsx
+++ components/common/Loader.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function Loader() {
   return (
-    <div className="flex justify-center items-center py-10">
-      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-green-600"></div>
+    <div
+      className="flex justify-center items-center py-10"
+    >
+      <div
+        className="h-12 w-12 animate-spin rounded-full border-4 border-gray-200 border-t-green-600"
+      ></div>
     </div>
   );
 }
