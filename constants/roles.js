60533587f0c3c87843c4fd8c5d533b3028b90a67
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-07-08 18:43:13 +0530

    Add display names for roles in Hindi
    Added Hindi display names for user roles.

--- constants/roles.js
+++ constants/roles.js
@@ -1,5 +1,24 @@
+// constants/roles.js
+
 export const ROLES = {
+
   CUSTOMER: "customer",
+
   DRIVER: "driver",
+
   ADMIN: "admin",
+
+};
+
+
+// Display Names
+
+export const ROLE_LABELS = {
+
+  customer: "किसान",
+
+  driver: "ड्राइवर",
+
+  admin: "एडमिन",
+
 };
