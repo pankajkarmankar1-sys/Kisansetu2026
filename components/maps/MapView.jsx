2c41a8b96170978aa77ca318371b8d19973144c8
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-07-08 18:51:07 +0530

    Refactor MapView to accept dynamic height prop

--- components/maps/MapView.jsx
+++ components/maps/MapView.jsx
@@ -1,32 +1,78 @@
+// components/maps/MapView.jsx
+
 import React from "react";
 
-export default function MapView({ children }) {
+
+export default function MapView({
+
+  children,
+
+  height = 400,
+
+}) {
+
+
   return (
+
     <div
+
       style={{
-        height: 400,
+
+        height,
+
+        width: "100%",
+
         borderRadius: 12,
+
         border: "1px solid #ddd",
+
         background: "#eef7ee",
+
         display: "flex",
+
         justifyContent: "center",
+
         alignItems: "center",
+
         position: "relative",
+
         overflow: "hidden",
+
       }}
+
     >
+
+
       <div
+
         style={{
+
           position: "absolute",
+
           top: 10,
+
           left: 10,
+
           fontWeight: "bold",
+
+          zIndex: 2,
+
         }}
+
       >
-        🗺️ Map View
+
+        🗺️ KisanSetu Map
+
       </div>
 
+
+
       {children}
+
+
+
     </div>
+
   );
+
 }
