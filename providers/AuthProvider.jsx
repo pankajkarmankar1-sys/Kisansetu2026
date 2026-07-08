801e16969d42dd691aea2f268bb9de3fe89ceea9
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-07-08 18:20:59 +0530

    Refactor AuthProvider to manage user state and loading

--- providers/AuthProvider.jsx
+++ providers/AuthProvider.jsx
@@ -1,11 +1,104 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { supabase } from "../lib/supabase";
 
-export default function AuthProvider({ children }) {
+
+export default function AuthProvider({
+  children
+}) {
+
+  const [user, setUser] = useState(null);
+
+  const [loading, setLoading] = useState(true);
+
+
+
+  useEffect(() => {
+
+    checkUser();
+
+
+    const {
+      data: listener
+    } =
+    supabase.auth.onAuthStateChange(
+      (
+        event,
+        session
+      ) => {
+
+        setUser(
+          session?.user || null
+        );
+
+      }
+    );
+
+
+    return () => {
+
+      listener.subscription.unsubscribe();
+
+    };
+
+
+  }, []);
+
+
+
+
+
+  async function checkUser(){
+
+    const {
+      data
+    } =
+    await supabase.auth.getSession();
+
+
+    setUser(
+      data.session?.user || null
+    );
+
+
+    setLoading(false);
+
+  }
+
+
+
+
+
+  async function logout(){
+
+    await supabase.auth.signOut();
+
+    setUser(null);
+
+  }
+
+
+
+
   return (
-    <AuthContext.Provider value={{}}>
+
+    <AuthContext.Provider
+
+      value={{
+        user,
+        loading,
+        logout,
+        setUser,
+      }}
+
+    >
+
       {children}
+
     </AuthContext.Provider>
+
   );
+
 }
