7c516bdad90bc3d9e76caa715fd5be21de60a97a
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-07-08 18:38:21 +0530

    Add notifications and payments to defaultDB

--- data/defaultDB.js
+++ data/defaultDB.js
@@ -1,16 +1,45 @@
 // data/defaultDB.js
 
 const defaultDB = {
+
   users: {},
+
   bookings: [],
+
   drivers: {},
+
+
   chats: {},
+
+
   khets: {},
+
+
   reviews: {},
+
+
   fleet: {},
+
+
   waitList: [],
+
+
   transfers: [],
+
+
   schedules: {},
+
+
+  notifications: [],
+
+
+  payments: [],
+
+
+  createdAt:
+    new Date().toISOString(),
+
 };
+
 
 export default defaultDB;
