5f31580fb124014af08b220e438ca802ed6f2fcc
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-07-08 18:19:53 +0530

    Refactor BookingProvider to manage booking state

--- providers/BookingProvider.jsx
+++ providers/BookingProvider.jsx
@@ -1,11 +1,35 @@
 "use client";
 
+import { useState } from "react";
 import { BookingContext } from "../context/BookingContext";
 
-export default function BookingProvider({ children }) {
+
+export default function BookingProvider({
+  children
+}) {
+
+  const [booking, setBooking] = useState(null);
+
+
+  const clearBooking = () => {
+    setBooking(null);
+  };
+
+
   return (
-    <BookingContext.Provider value={{}}>
+
+    <BookingContext.Provider
+      value={{
+        booking,
+        setBooking,
+        clearBooking,
+      }}
+    >
+
       {children}
+
     </BookingContext.Provider>
+
   );
+
 }
