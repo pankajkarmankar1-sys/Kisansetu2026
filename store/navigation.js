0cd1f751b07fef1d8ec73e589e61ee488646c487
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-07-08 17:52:58 +0530

    Refactor navigation constants for consistency

--- store/navigation.js
+++ store/navigation.js
@@ -2,30 +2,29 @@
 
 export const SCREENS = {
 
+  // Common
   ROLE: "role",
-
   LOGIN: "login",
-
-  REGISTER: "reg",
-
+  REGISTER: "register",
   HOME: "home",
-
   PROFILE: "profile",
-
-  BOOK: "book",
-
-  PAYMENT: "payment",
-
   HISTORY: "history",
-
   CHAT: "chat",
 
+  // Customer
+  BOOK: "book",
+  PAYMENT: "payment",
+
+  // Driver
   DRIVER_HOME: "driverHome",
-
   DRIVER_BOOKINGS: "driverBookings",
-
   DRIVER_EARNINGS: "driverEarnings",
+  DRIVER_HISTORY: "driverHistory",
+  DRIVER_PROFILE: "driverProfile",
 
+  // Admin
   ADMIN: "admin",
+  ADMIN_BOOKINGS: "adminBookings",
+  ADMIN_DRIVERS: "adminDrivers",
 
 };
