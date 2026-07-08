d87a0541582163f810f134e936ac20afb3cc8c6e
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-07-08 18:18:57 +0530

    Refactor DriverProvider to manage driver state

--- providers/DriverProvider.jsx
+++ providers/DriverProvider.jsx
@@ -1,11 +1,29 @@
 "use client";
 
+import { useState } from "react";
 import { DriverContext } from "../context/DriverContext";
 
-export default function DriverProvider({ children }) {
+
+export default function DriverProvider({
+  children
+}) {
+
+  const [driver, setDriver] = useState(null);
+
+
   return (
-    <DriverContext.Provider value={{}}>
+
+    <DriverContext.Provider
+      value={{
+        driver,
+        setDriver,
+      }}
+    >
+
       {children}
+
     </DriverContext.Provider>
+
   );
+
 }
