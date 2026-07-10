acdf6a8d1ffe35ed570d56488258fb7ccdc102cb
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-07-10 06:50:46 +0530

    Fix import path for DriverDashboard component

--- pages/driver/index.jsx
+++ pages/driver/index.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { supabase } from "../../lib/supabase";
-
-import DriverDashboard from "../../components/driver/Driver Dashboard";
+import DriverDashboard from "../../components/driver/DriverDashboard";
 
 
 
