957b9715a64a17320f2b8e70e67ccbafe4263528
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-07-13 04:52:02 +0530

    Update import path for LocationSelector component

--- components/customer/AddFarm.jsx
+++ components/customer/AddFarm.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { supabase } from "../../lib/supabase";
-import LocationSelector from "./LocationSelector";
+import LocationSelector from "../maps/LocationSelector";
 
 export default function AddFarm({
   onSaved,
