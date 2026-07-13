49e784f818aecfbe59be5cc4e02e942cb97d45a3
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-07-13 10:18:58 +0530

    Integrate MUI theme provider in _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,32 @@
 import "../styles/globals.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2E7D32",
+    },
+    secondary: {
+      main: "#F9A825",
+    },
+    background: {
+      default: "#F4F7F6",
+    },
+  },
+  shape: {
+    borderRadius: 18,
+  },
+  typography: {
+    fontFamily: "Inter, sans-serif",
+  },
+});
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
