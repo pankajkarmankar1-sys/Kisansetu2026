5be0c1cfb117941b118aa84b6669c5fd63d81962
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-07-13 19:33:03 +0530

    Remove ThemeProvider and CssBaseline from MyApp
    Removed Material-UI theme provider and CssBaseline from MyApp component.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,32 +1,5 @@
 import "../styles/globals.css";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#2E7D32",
-    },
-    secondary: {
-      main: "#F9A825",
-    },
-    background: {
-      default: "#F4F7F6",
-    },
-  },
-  shape: {
-    borderRadius: 18,
-  },
-  typography: {
-    fontFamily: "Inter, sans-serif",
-  },
-});
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
